be8ee92c9c2aa509272bdbbb2c39a9bb5cbd52c8
Arpit Patel <34958123+arpit194@users.noreply.github.com>
2022-05-01 14:30:58 +0530

    Create variables.js

---
+++ src/store/variables.js
@@ -0,0 +1,17 @@
+let prod = 0;
+
+let env;
+let dbURL;
+let API_KEY;
+
+if (prod) {
+  env = "test";
+  dbURL = "firebase realtime db url for test";
+  API_KEY = "firebase api key for test ";
+} else {
+  env = "prod";
+  dbURL = "firebase realtime db url for prod";
+  API_KEY = "firebase api key for prod";
+}
+
+export { env, dbURL, API_KEY };
